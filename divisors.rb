f39adc51aed85c36d3089aebe06114e540c46b3f
Alexeev Grigorii <grand.alexeev@gmail.com>
2015-12-09 00:25:41 +0200

    Update divisors.rb

--- divisors.rb
+++ divisors.rb
@@ -2,9 +2,7 @@ require 'mathn'
 
 def divisors(number)
   primes, powers = number.prime_division.transpose
-  
   exponents = powers.map{|p| (0..p).to_a}
-  
   divisors = exponents.shift.product(*exponents).map do 
       |powers|
       primes.zip(powers).map{|prime, power| prime ** power}.reduce(:*)
